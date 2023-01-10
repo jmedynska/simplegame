b5d787a7dba6c79e29737e53beeea0e49c29c78b
Julia Medyńska <julia.medynska1@gmail.com>
2023-01-10 13:40:35 +0100

    Display image

---
+++ HelloSprite.py
@@ -0,0 +1,19 @@
+import pygame
+pygame.init()
+
+screen = pygame.display.set_mode((800,600), 0, 32) #Main window
+sprite1 = pygame.image.load('./simplegame/images/football.png') #Ball image
+
+pygame.display.set_caption("Hello Sprite") #Caption on main window
+screen.fill((0, 0, 0)) #Black background
+
+game_over = False
+
+#Main game loop
+while not game_over:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            game_over = True
+    screen.blit(sprite1, (0, 0))
+    pygame.display.update()
+pygame.quit()
