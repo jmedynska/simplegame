94d5a5a526b9873479e03821760f798836744d45
Julia Medyńska <julia.medynska1@gmail.com>
2023-01-10 14:00:01 +0100

    Add butterfly in the center

--- HelloSprite.py
+++ HelloSprite.py
@@ -1,8 +1,12 @@
 import pygame
 pygame.init()
-
-screen = pygame.display.set_mode((800,600), 0, 32) #Main window
+screen_width = 700
+screen_height = 700
+screen = pygame.display.set_mode((screen_width, screen_height), 0, 32) #Main window
 sprite1 = pygame.image.load('./simplegame/images/football.png') #Ball image
+sprite2 = pygame.image.load('./simplegame/images/butterfly.png')
+sprite2_width = sprite2.get_width()
+sprite2_height = sprite2.get_height()
 
 pygame.display.set_caption("Hello Sprite") #Caption on main window
 screen.fill((0, 0, 0)) #Black background
@@ -14,6 +18,7 @@ while not game_over:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             game_over = True
-    screen.blit(sprite1, (384, 284))
+    #screen.blit(sprite1, (384, 284)) # ball in the center of the screen
+    screen.blit(sprite2, (screen_width/2-sprite2_width/2, screen_height/2 - sprite2_height/2))
     pygame.display.update()
 pygame.quit()
