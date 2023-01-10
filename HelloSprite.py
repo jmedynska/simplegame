4555024e0974610c9ddb5e0a094e960fa51e6aea
Julia Medyńska <julia.medynska1@gmail.com>
2023-01-10 13:49:18 +0100

    Center the ball

--- HelloSprite.py
+++ HelloSprite.py
@@ -14,6 +14,6 @@ while not game_over:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             game_over = True
-    screen.blit(sprite1, (0, 0))
+    screen.blit(sprite1, (384, 284))
     pygame.display.update()
 pygame.quit()
