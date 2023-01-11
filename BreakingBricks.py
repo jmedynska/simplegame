ff370b0e95999691668543e806d6f0d6cb710790
Julia Medyńska <julia.medynska1@gmail.com>
2023-01-11 14:41:54 +0100

    Add bricks and moving bat

---
+++ BreakingBricks.py
@@ -0,0 +1,66 @@
+import pygame
+from pygame.locals import *
+
+pygame.init()
+
+screen = pygame.display.set_mode((800,600))
+pygame.display.set_caption("Breakin' Bricks")
+
+#bat
+bat = pygame.image.load('./simplegame/images/paddle.png')
+bat = bat.convert_alpha()
+bat_rect = bat.get_rect()
+bat_rect[1] = screen.get_height() - 100
+
+#ball
+ball = pygame.image.load('./simplegame/images/football.png')
+ball = ball.convert_alpha()
+ball_rect = ball.get_rect()
+
+
+#brick
+brick = pygame.image.load('./simplegame/images/brick.png')
+brick = brick.convert_alpha()
+brick_rect = brick.get_rect()
+bricks = []     #empty list
+brick_rows = 5
+brick_gap = 10
+brick_cols = screen.get_width() // (brick_rect[2]+brick_gap) #how many bricks will fit into the screen
+side_gap = (screen.get_width() - brick_cols * (brick_rect[2]+brick_gap) + brick_gap)//2
+
+for y in range(brick_rows):
+    brickY = y * (brick_rect[3] + brick_gap)
+    for x in range(brick_cols):
+        brickX = x * (brick_rect[2] + brick_gap)  + side_gap
+        bricks.append((brickX,brickY))
+
+
+
+clock = pygame.time.Clock()
+game_over = False
+
+while not game_over:
+    dt = clock.tick(50)
+    screen.fill((0,0,0))
+
+    for b in bricks:
+        screen.blit(brick, b)
+    pressed = pygame.key.get_pressed()
+    if pressed[K_RIGHT]:
+        bat_rect[0]+=0.5* dt
+    if pressed[K_LEFT]:
+        bat_rect[0]-=0.5* dt
+    if pressed[K_SPACE]:
+        bat_rect[0]
+    if bat_rect[0] > (screen.get_width()-bat_rect[2]):
+        bat_rect[0] = screen.get_width()-bat_rect[2]
+    if bat_rect[0] < 0:
+        bat_rect[0] = 0
+
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            game_over = True
+    screen.blit(bat, (bat_rect[0],bat_rect[1]))
+    pygame.display.update()
+        
+pygame.quit()
